62180c7e4b18810f25f8e1104b7b1b1e5ca007b9
ana <anamariaguzmanh1996@gmail.com>
2024-10-02 21:28:05 -0500

    feat: activity

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { ApiCoffeeCarruselBasic } from './components/AppiCoffeBasicCarrusel'
+import { ApiPokemonCard } from './components/AppiPokemonCard'
+import { CatGalleryFetch } from './components/CatGalleryFetch'
+
+
+function App() {
+  return (
+    <div>
+      <h1>
+        {/* <ApiCoffeeCarruselBasic />
+        <CatGalleryFetch /> */}
+      < ApiPokemonCard/>
+      </h1>
+    </div>    
+  )
+}
+export default App
